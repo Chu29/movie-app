307d3faa7d1a152642509d46f73b7310cdd934cc
Chu A. <chu.amk22@gmail.com>
2025-11-19 08:41:10 +0100

    feat(ui): Implement Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { useState } from "react";
+import Search from "./components/Search";
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div>App</div>
-  )
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+      </div>
+    </main>
+  );
 }
