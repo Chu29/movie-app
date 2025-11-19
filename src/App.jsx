c6ae66ffe5023f799cdd37f087bbb7b01cc85163
Chu A. <chu.amk22@gmail.com>
2025-11-19 08:46:47 +0100

    feat(app): Integrate the movie bd api and fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
-import { useState } from "react";
 import Search from "./components/Search";
+import { useState, useEffect } from "react";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 export default function App() {
   const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchMovies = async (query = "") => {
+    try {
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+      const data = await response.json();
+    } catch (error) {
+      console.log(`Error fetching movies: ${error}`);
+    }
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main>
@@ -16,6 +48,10 @@ export default function App() {
           </h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+
+        <section className="all-movies">
+          <h2 className="mt-10">All Movies</h2>
+        </section>
       </div>
     </main>
   );
