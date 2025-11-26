203ce0c9fc54c86e64b08de0da68b346a903c714
Chu A. <chu.amk22@gmail.com>
2025-11-26 17:05:21 +0100

    chore: configure ky and implement handle function to fetch movies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,13 @@
 export const MOVIE_POSTER_PATH = "https://image.tmdb.org/t/p/w500/";
 export const APPWRITE_API_ENDPOINT = "https://fra.cloud.appwrite.io/v1";
+export const API_BASE_URL = "https://api.themoviedb.org/3";
+
+export const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
